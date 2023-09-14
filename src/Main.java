0644932e711024d5c1c2f99b734875baed88f03d
Kostya <Goblinbos@mail.ru>
2023-09-14 21:23:53 +0300

    Methods

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,31 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int c = Sum(a, b);
-        System.out.println("%d + %d = %d".formatted(a, b, c) );
-        System.out.println("%d - %d = %d".formatted(a, b, Diff(a, b)) );
-        System.out.println("%d * %d = %d".formatted(a, b, Prod(a, b)) );
-        System.out.println("%d / %d = %f".formatted(a, b, Div(a, b)) );
+        int c = Math.Sum(a, b);
+        System.out.println("%d + %d = %d".formatted(a, b, c));
+        System.out.println("%d - %d = %d".formatted(a, b, Math.Diff(a, b)));
+        System.out.println("%d * %d = %d".formatted(a, b, Math.Prod(a, b)));
+        System.out.println("%d / %d = %f".formatted(a, b, Math.Div(a, b)));
     }
-     static int Sum (int a, int b)
-    {
+
+}
+
+class Math {
+    static int Sum(int a, int b) {
         int c = a + b;
         return c;
 
     }
-    static int Diff(int a, int b)
-    {
+
+    static int Diff(int a, int b) {
         return a - b;
     }
-    static int Prod ( int a, int b)
-    {
+
+    static int Prod(int a, int b) {
         return a * b;
     }
-    static double Div (int a, int b)
-    {
-        return (double)a / b;
+
+    static double Div(int a, int b) {
+        return (double) a / b;
     }
 }
